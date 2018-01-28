d2a14cacec05e7ba689d7b2ff5097044846860a7
yammark <theyammark@gmail.com>
2018-01-28 13:21:54 +0100

    ordnet adaptive music. funker i testing grounds

--- Assets/Scripts/TimeStopController.cs
+++ Assets/Scripts/TimeStopController.cs
@@ -7,7 +7,7 @@ public class TimeStopController : MonoBehaviour{
     public static float currentTime = 0;
     private static float prevTime = 0;
     public float deltaTimeScale;
-    public static float timeScale = 1;
+    public static float timeScale = 0;
     public List<float> prevScales = new List<float>();
 
     public static TimeStopController instance;
